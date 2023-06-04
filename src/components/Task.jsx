80ffe620e12a2990d870e05e7d56e3dca2a75eb3
leonardo fierro <planeaweb@gmail.com>
2023-06-04 17:15:15 -0500

    Corregi error en id

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,12 +3,13 @@ import './Task.css';
 
 function Task(props) {
   const [completed, setCompleted] = useState(props.completed);
-
+ 
   useEffect(() => {
     // Cargar el estado de completado de la tarea desde localStorage
     const storedCompleted = localStorage.getItem(props.name);
     if (storedCompleted) {
       setCompleted(JSON.parse(storedCompleted));
+     
     }
   }, [props.name]);
 
@@ -17,25 +18,50 @@ function Task(props) {
     const newCompleted = !completed;
     setCompleted(newCompleted);
     localStorage.setItem(props.name, JSON.stringify(newCompleted));
-  };
+    
+    };
+   
+    return (
+      <li>
+        <h1>
+        <input id={`checkbox-${props.index}`} type="checkbox" checked={completed} onChange={handleCheckboxChange} />
+        <span className="task-name" style={{ textDecoration: completed ? 'line-through' : 'none' }}>
+          {props.name}
+        </span>
+        <button id={`edit-button-${props.index}`} className="edit-button" type="submit">
+          <img src="/src/components/imagenes/agregar.png" alt="Modificar" className="icon" />
+          Modificar
+        </button>
+        <button id={`delete-button-${props.index}`} className="delete-button" type="submit">
+          <img src="/src/components/imagenes/eliminar2.gif" alt="Eliminar" className="icon" />
+          Eliminar
+        </button>
+        </h1>
+      </li>
+    );
+  }
+   export default Task;
+  
+  
+  
+  
+  
+  
 
-  return (
-    <li>
+    
+  /*return (
+   <li>
       <h1>
-        <input type="checkbox" checked={completed} onChange={handleCheckboxChange}
-        />
-
-        <span style={{ textDecoration: completed ? 'line-through' : 'none' }}>{props.name}</span>
-
-        <button className="icon-button" type="submit">
-          <img src="editar.gif" alt="Modificar" className="icon" />
-        </button>
-
-        <button type="submit">
-          <img src="eliminar.gif" />Eliminar
-        </button>
+        <input id={`checkbox-${uniqueId}`} type="checkbox" checked={completed} onChange={handleCheckboxChange}/>
+        <span className="task-name" style={{ textDecoration: completed ? 'line-through' : 'none' }}>{props.name}</span>
+        <button id={`edit-button-${uniqueId}`} className="edit-button" type="submit"><img src="/src/components/imagenes/agregar.png" alt="Modificar" className="icon" />Modificar</button>
+        <button id={`delete-button-${uniqueId}`} className="delete-button" type="submit"> <img src="/src/components/imagenes/eliminar2.gif" alt="Eliminar" className="icon"/>Eliminar </button>
       </h1>
-    </li>
+      
+   </li>
   );
+
+
 }
 export default Task;
+*/
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Task from './Task';
+import './TaskList.css';
 
 function TaskList() {
   const tasks = [
-    { id: 1, name: 'Atender cliente 8 am   ', completed: false},
-    { id: 2, name: 'Instalar sistemas de camaras de seguridad   ', completed: "" },
-    { id: 3, name: 'Realizar Actividades de ADA SCHOOL  ', completed: "" },
-    { id: 4, name: 'Llamar a mi Madre   ', completed: "" },
-    { id: 4, name: 'Sacar la basura de los Gatos    ', completed: ""}
+    { id: 1, name: 'Atender cliente 8 am', completed: false },
+    { id: 2, name: 'Instalar sistemas de cámaras de seguridad', completed: "" },
+    { id: 3, name: 'Realizar Actividades de ADA SCHOOL', completed: "" },
+    { id: 4, name: 'Llamar a mi Madre', completed: "" },
+    { id: 5, name: 'Sacar la basura de los Gatos', completed: "" }
   ];
 
   return (
-    <ul>
-     <div class="task-list">
-    <div class="task">
-      {tasks.map(task => (
-        <Task key={task.id} name={task.name} completed={task.completed} />
-      ))}
+    <div className="task-list">
+      <h1>Hola Mundo</h1>
+      <ul>
+        {tasks.map((task, index) => (
+          <Task key={task.id} name={task.name} completed={task.completed} index={index} />
+        ))}
+      </ul>
     </div>
-    </div>
-    </ul>
   );
 }
 
