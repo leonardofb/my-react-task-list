4d38b33b5f4b0c0dd3c5b90c8d38e941710c6113
leonardo fierro <planeaweb@gmail.com>
2023-06-15 04:04:15 -0500

    Ya logre el Hola mundo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,37 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import TaskList from './pages/TaskList';
+/*import HomePage from './pages/HomePage';
+import EventsPage from './pages/EventsPage';
+*/
+
 export function Menu() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/events">Events</a>
-        </li>
-      </ul>
-    </nav>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/events">Events</Link>
+          </li>
+          <li>
+            <Link to="/listas">Listas</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          {/* Contenido de la página Home */}
+        </Route>
+        <Route path="/events">
+          {/* Contenido de la página Events */}
+        </Route>
+        <Route path="/listas">
+          <TaskList />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
