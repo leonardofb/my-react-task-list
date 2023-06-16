9eaba6b83c9eddb585dabd2562f220973340a504
leonardo fierro <planeaweb@gmail.com>
2023-06-16 09:59:28 -0500

    Update main.jsx
    Este Commit lo esto generando desde la app de githup haber que diferencia hay en el uso de esta app o por codigo visual

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <div>Hello world! </div>,
   },
 ]);
 
