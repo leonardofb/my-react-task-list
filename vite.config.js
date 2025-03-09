c06a6669ac90c7d638bebfd044de46811e4a0e62
Leonardo Fierro <poliservicios@gmail.com>
2025-03-08 20:03:23 -0500

    Actualice parte 1 que trabae en gh-pages y local falta las rutas

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/my-react-task-list/' : '/',
+  build: {
+    assetsInlineLimit: 0, // Evita que imágenes pequeñas se conviertan en Base64
+  }
+});
